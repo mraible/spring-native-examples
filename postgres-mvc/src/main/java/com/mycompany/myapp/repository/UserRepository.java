4a8db3cc4fa3f46a4ce25969a85b1d964d10f620
Matt Raible <matt.raible@okta.com>
2021-09-29 11:13:35 -0700

    Initial version of PostgresMVC generated by generator-jhipster@7.2.0

---
+++ postgres-mvc/src/main/java/com/mycompany/myapp/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.mycompany.myapp.repository;
+
+import com.mycompany.myapp.domain.User;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the {@link User} entity.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findOneByLogin(String login);
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByLogin(String login);
+
+    Page<User> findAllByIdNotNullAndActivatedIsTrue(Pageable pageable);
+}
