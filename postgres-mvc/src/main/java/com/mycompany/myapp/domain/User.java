993d143506fab88586e66a06f95ff057244592c9
Matt Raible <matt.raible@okta.com>
2021-12-13 23:47:38 -0700

    Fix JPA + login + EntityListeners

--- postgres-mvc/src/main/java/com/mycompany/myapp/domain/User.java
+++ postgres-mvc/src/main/java/com/mycompany/myapp/domain/User.java
@@ -13,12 +13,14 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.annotations.BatchSize;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 /**
  * A user.
  */
 @Entity
 @Table(name = "jhi_user")
+@EntityListeners(AuditingEntityListener.class)
 public class User extends AbstractAuditingEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
